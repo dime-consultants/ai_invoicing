b01b8b3e28d6539e80bcb8ef8f20ce5aa3ddbb90
charlesdete <charlesdete52@gmail.com>
2026-08-19 10:16:27 +0300

    unstaged changes in the settings.py

--- config/settings.py
+++ config/settings.py
@@ -200,6 +200,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://localhost:8000",
     "https://kuehne.dimeconsultants.africa",
     "https://guardian.dimeconsultants.africa",
 ]
@@ -225,6 +226,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://kuehne.dimeconsultants.africa",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "http://localhost:8000",
     "https://guardian.dimeconsultants.africa",
 ]
 
